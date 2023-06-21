36f318c381855eb1128fbdf505c5d3a9fef178af
ZahraaTayyar <zahraatayyar@gmail.com>
2023-06-21 11:03:20 +0100

    Deleted commented out code

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,26 +1,7 @@
-// import { useEffect, useState } from "react";
-import "./Header.js";
 import "./Home.css";
 import Header from "./Header.js";
 
 export function Home() {
-	// const [message, setMessage] = useState("Loading...");
-
-	// useEffect(() => {
-	// 	fetch("/api")
-	// 		.then((res) => {
-	// 			if (!res.ok) {
-	// 				throw new Error(res.statusText);
-	// 			}
-	// 			return res.json();
-	// 		})
-	// 		.then((body) => {
-	// 			setMessage(body.message);
-	// 		})
-	// 		.catch((err) => {
-	// 			console.error(err);
-	// 		});
-	// }, []);
 
 	return (
 		<main role="main">
