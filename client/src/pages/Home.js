3a52f7966fc981aa42d65fad37175a188df00d0a
Hadi Allahverdi Pournigjeh <108987953+hadipournigjeh@users.noreply.github.com>
2023-06-21 14:09:55 +0100

    output-component (#14)

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,43 @@
 import "./Home.css";
 import Header from "./Header.js";
+import React, { useState } from "react";
+import Input from "./Input.js";
+import Output from "./Output.js";
 
 export function Home() {
+
+	const [inputValue, setInputValue] = useState("");
+	const [showOutput, setShowOutput] = useState(false);
+
+  const handleInputChange = (value) => {
+    setInputValue(value);
+  };
+
+  const handleSubmit = () => {
+    setShowOutput(true);
+  };
+
+  const handleReset = () => {
+    setShowOutput(false);
+    setInputValue("");
+  };
 
 	return (
 		<main role="main">
 			<div>
 				<Header />
 			</div>
+			<div className="app">
+				{!showOutput ? (
+					<div className="input-container">
+					<Input onInputChange={handleInputChange} onSubmit={handleSubmit} />
+					</div>
+				) : (
+					<div className="input-container">
+					<Output inputValue={inputValue} onReset={handleReset} />
+					</div>
+				)}
+    </div>
 		</main>
 	);
 }
