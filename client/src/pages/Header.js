527d48bbd718f3501bf7b59039caefe9d5a6568a
lexiisying <nyxxing0824@gmail.com>
2023-06-21 10:46:54 +0100

    created header

---
+++ client/src/pages/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+
+const Header = () => {
+	return (
+		<header className="header">
+			<h1 className="header__title">WordWise</h1>
+			<nav className="header__nav">
+				<Link className="header__nav-link" to="/about">
+					About
+				</Link>
+				<Link className="header__nav-link" to="/login">
+					Login
+				</Link>
+				<Link className="header__nav-link" to="/register">
+					Register
+				</Link>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
