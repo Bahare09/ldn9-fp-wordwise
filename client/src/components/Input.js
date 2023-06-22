77353d4feaa2bf8c24ea574f68e035e22f8d8fd1
Bahare <108987748+Bahare09@users.noreply.github.com>
2023-06-22 10:42:01 +0100

    cleaning (#18)
    Well done Bahare :)

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./style.css";
+import "./InputOutput.css";
 
 const Input = ({ onInputChange, onSubmit }) => {
   const [inputValue, setInputValue] = useState("");
--- client/src/components/Output.js
+++ client/src/components/Output.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./style.css";
+import "./InputOutput.css";
 
 const Output = ({ inputValue, onReset }) => {
   return (
