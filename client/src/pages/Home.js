958e6f343e7146b2df50d5876ad149dd6b6ea211
Zahraa <113244988+ZahraaTayyar@users.noreply.github.com>
2023-06-23 17:42:19 +0100

    mock footer done (#17)
    * mock footer done
    
    * mock footer 2.0
    
    * Delete package.json
    
    * Delete package-lock.json
    
    ---------
    
    Co-authored-by: Bahare <108987748+Bahare09@users.noreply.github.com>

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Header from "../components/Header";
 import Input from "../components/Input";
 import Output from "../components/Output";
+import Footer from "./Footer";
 import "./Home.css";
 
 const Home = () => {
@@ -34,9 +35,14 @@ const Home = () => {
 					</div>
 				)}
 			</div>
+			<div>
+				<Footer />
+			</div>
 		</main>
 	);
 };
 
 export default Home;
+
+
 
