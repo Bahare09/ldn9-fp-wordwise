fd54abb3e274f626f6fb8e4f2414abe4f4777b4c
Bahare <108987748+Bahare09@users.noreply.github.com>
2023-06-23 14:28:38 +0100

    Update Input.js

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -2,50 +2,50 @@ import React, { useState } from "react";
 import "./InputOutput.css";
 
 const Input = ({ onSubmit }) => {
-const [inputValue, setInputValue] = useState("");
+	const [inputValue, setInputValue] = useState("");
 
-const handleInputChange = (e) => {
-setInputValue(e.target.value);
-};
+	const handleInputChange = (e) => {
+		setInputValue(e.target.value);
+	};
 
-const handleSubmit = async (e) => {
-e.preventDefault();
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-try {
-const response = await fetch("http://localhost:3000/api", {
-method: "POST",
-headers: {
-"Content-Type": "application/json",
-},
-body: JSON.stringify({ input: inputValue }),
-});
+		try {
+			const response = await fetch("http://localhost:3000/api", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({ input: inputValue }),
+			});
 
-if (response.ok) {
-const data = await response.json();
-onSubmit(data);
-} else {
-console.log("Error: " + response.status);
-}
-} catch (error) {
-console.log(error);
-}
+			if (response.ok) {
+				const data = await response.json();
+				onSubmit(data);
+			} else {
+				console.log("Error: " + response.status);
+			}
+		} catch (error) {
+			console.log(error);
+		}
 
-setInputValue("");
-};
+		setInputValue("");
+	};
 
-return (
-<div className="input-container">
-<textarea
-value={inputValue}
-onChange={handleInputChange}
-placeholder="Enter something..."
-className="input-field"
-/>
-<button type="submit" className="submit-button" onClick={handleSubmit}>
-&#10148;
-</button>
-</div>
-);
+	return (
+		<div className="input-container">
+			<textarea
+				value={inputValue}
+				onChange={handleInputChange}
+				placeholder="Enter something..."
+				className="input-field"
+			/>
+			<button type="submit" className="submit-button" onClick={handleSubmit}>
+				&#10148;
+			</button>
+		</div>
+	);
 };
 
 export default Input;
