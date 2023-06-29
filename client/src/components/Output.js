ae0df30e26ef79ea2778452f040abd3ee5bf99e1
Hadi Allahverdi Pournigjeh <108987953+hadipournigjeh@users.noreply.github.com>
2023-06-29 23:14:43 +0100

    made alternative box

--- client/src/components/Output.js
+++ client/src/components/Output.js
@@ -1,15 +1,21 @@
 import React from "react";
 import "./InputOutput.css";
 import CopyButton from "./CopyButton.js";
+import TextToSpeechButton from "./TextToSpeechButton";
 
 const Output = ({ outputValue, onReset }) => {
 	return (
-		<div>
-			<textarea className="output-field" value={outputValue} readOnly />
-			<CopyButton text={outputValue} />
-			<button onClick={onReset} className="back-button">
-				&#10149;
-			</button>
+		<div className="all-output-container">
+			<div>
+				<textarea className="output-field" value={outputValue} readOnly />
+				<button onClick={onReset} className="back-button">
+					&#10149;
+				</button>
+			</div>
+			<div className="volume-copy-container">
+				<CopyButton text={outputValue} />
+				<TextToSpeechButton />
+			</div>
 		</div>
 	);
 };
