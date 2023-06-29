a3d79ef220c3e0d76985b623da97d51bf55749b6
lexiisying <nyxxing0824@gmail.com>
2023-06-29 01:19:16 +0100

    update layout

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -14,40 +14,41 @@ const About = () => {
 	const images = [bahare, hadi, zahraa, lexi, erin, rob, dom];
 
 	return (
-		<main role="main" className="about-page">
-			<div>
-				<Header currentPage="about" />
-			</div>
-			<div className="about-page">
-				<h2>About Us</h2>
-				<p>
-					WordWise is an innovative platform aimed at enhancing and enriching
-					your English skills. Our interactive tools and engaging content
-					provide an effective and fun way to learn new words and improve your
-					overall communication.
-				</p>
-				<div className="elements-container">
-					{images.map((element, index) => (
-						<div key={index} className="element-box">
-							<div className="picture-container">
-								<img src={element} alt={`element${index + 1}`} />
+		<div>
+			<Header currentPage="about" />
+			<main role="main" className="about-page">
+				<div className="content">
+					<div className="intro-container">
+						<h2>About us</h2>
+						<p>
+							WordWise is an innovative platform aimed at enhancing and
+							enriching your English skills. Our interactive tools and engaging
+							content provide an effective and fun way to learn new words and
+							improve your overall communication.
+						</p>
+						<h3>Meet the team</h3>
+					</div>
+					<div className="elements-container">
+						{images.map((element, index) => (
+							<div key={index} className="element-box">
+								<div className="picture-container">
+									<img src={element} alt={`element${index + 1}`} />
+								</div>
+								<div className="social-media-links">
+									<a href="#linkedin" target="_blank" rel="">
+										LinkedIn
+									</a>
+									<a href="#github" target="_blank" rel="">
+										Github
+									</a>
+								</div>
 							</div>
-							<div className="social-media-links">
-								<a href="#linkedin" target="_blank" rel="">
-									LinkedIn
-								</a>
-								<a href="#github" target="_blank" rel="">
-									Github
-								</a>
-							</div>
-						</div>
-					))}
+						))}
+					</div>
 				</div>
-			</div>
-			<div>
-				<Footer />
-			</div>
-		</main>
+			</main>
+			<Footer />
+		</div>
 	);
 };
 
