56489674d22c67aeb3297b793870856f6e5a32d0
Ying X <105919943+LexiisYing@users.noreply.github.com>
2023-06-29 10:36:28 +0100

    fix import bug on footer (#40)

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
-import Footer from "./Footer";
+import Footer from "../components/Footer";
 
 const About = () => {
 	const elements = [
