8af36ef7ebc1b97e8c8e00231e87b4fe24538c1a
ZahraaTayyar <zahraatayyar@gmail.com>
2023-06-29 21:53:49 +0100

    design

--- client/src/components/Output.js
+++ client/src/components/Output.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./InputOutput.css";
+import CopyButton from "./CopyButton.js";
 import TextToSpeechButton from "./TextToSpeechButton";
 
 const Output = ({ outputValue, onReset }) => {
 	return (
 		<div>
 			<textarea className="output-field" value={outputValue} readOnly />
+			<CopyButton text={outputValue} />
 			<button onClick={onReset} className="back-button">
 				&#10149;
 			</button>
