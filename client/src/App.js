e9fe4f24398536dbe39a36e27081e2790edc633e
Zahraa <113244988+ZahraaTayyar@users.noreply.github.com>
2023-06-30 20:58:31 +0100

    header-footer Router (#47)
    * header-footer Router
    
    * put header back in home.js to fix bug - can work on this later

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
-
+// import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => (
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about" element={<About />} />
-	</Routes>
+	<Router>
+		<Routes>
+			<Route path="/" element={<Home />} />
+			<Route path="/about" element={<About />} />
+		</Routes>
+		<Footer />
+	</Router>
 );
 
 export default App;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
-import Header from "../components/Header";
 import Input from "../components/Input";
 import Output from "../components/Output";
-import Footer from "../components/Footer";
 import "./Home.css";
 import "../components/InputOutput.css";
+import Header from "../components/Header";
 
 const Home = () => {
 	const [showOutput, setShowOutput] = useState(false);
@@ -78,9 +77,6 @@ const Home = () => {
 						</div>
 					</div>
 				)}
-			</div>
-			<div>
-				<Footer />
 			</div>
 		</main>
 	);
