6963619bbda87401e80de447296a4995680b4b07
lexiisying <nyxxing0824@gmail.com>
2023-07-01 23:18:32 +0100

    create alternative button

---
+++ client/src/components/AlternativeButton.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const AlternativeButton = ({ outputValue, setAlternativeValue }) => {
+	const getAlternatives = async () => {
+		try {
+			const response = await fetch("http://localhost:3000/api", {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ input: outputValue }),
+			});
+			if (response.ok) {
+				const alternative = await response.json();
+				setAlternativeValue(alternative);
+			} else {
+				console.log("Error: " + response.status);
+			}
+		} catch (error) {
+			console.error("Failed to fetch alternatives:", error);
+		}
+	};
+
+	return (
+		<button onClick={getAlternatives} className="alternative-button">
+			Get Alternatives
+		</button>
+	);
+};
+
+export default AlternativeButton;
