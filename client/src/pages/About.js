52acac6fb573d0fbd35ba7714ebe48a17bf50bcf
Ying X <105919943+LexiisYing@users.noreply.github.com>
2023-07-01 09:56:58 +0100

    Lexi-update-about-page-layout (#49)
    * update
    
    * update about.js&css
    
    * import LinkedInIcon
    
    * import GitHubIcon
    
    * code refactor

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -8,18 +8,55 @@ import zahraa from "../images/zahraa.jpg";
 import lexi from "../images/lexi.jpg";
 import rob from "../images/rob.jpg";
 import dom from "../images/dom.jpg";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
 
+
+const ElementBox = ({ image, description, linkedin, github, index }) => (
+	<div key={index} className="element-box">
+		<div className="picture-container">
+			<img src={image} alt={`element${index + 1}`} />
+		</div>
+		<p>{description}</p>
+		<div className="social-media-links">
+			<a href={linkedin} target="_blank" rel="noreferrer">
+				<LinkedInIcon />
+			</a>
+			<a href={github} target="_blank" rel="noreferrer">
+				<GitHubIcon />
+			</a>
+		</div>
+	</div>
+);
 
 const About = () => {
-	const images = [bahare, hadi, zahraa, lexi, erin, rob, dom];
+	const images = [bahare, hadi, zahraa, lexi, rob, dom, erin];
 	const descriptions = [
 		"Bahare - Developer",
 		"Hadi - Developer",
 		"Zahraa - Developer",
 		"Lexi - Developer",
-		"Erin - PO",
 		"Rob - PM",
 		"Dom - Tech Mentor",
+		"Erin - PO",
+	];
+	const linkedins = [
+		"linkedin1",
+		"linkedin2",
+		"linkedin3",
+		"linkedin4",
+		"linkedin5",
+		"linkedin6",
+		"linkedin7",
+	];
+	const githubs = [
+		"github1",
+		"github2",
+		"github3",
+		"github4",
+		"github5",
+		"github6",
+		"github7",
 	];
 
 	return (
@@ -38,21 +75,27 @@ const About = () => {
 						<h3>Meet the team ⭐</h3>
 					</div>
 					<div className="elements-container">
-						{images.map((element, index) => (
-							<div key={index} className="element-box">
-								<div className="picture-container">
-									<img src={element} alt={`element${index + 1}`} />
-								</div>
-								<p>{descriptions[index]}</p>
-								<div className="social-media-links">
-									<a href="#linkedin" target="_blank" rel="">
-										LinkedIn
-									</a>
-									<a href="#github" target="_blank" rel="">
-										Github
-									</a>
-								</div>
-							</div>
+						{images.slice(0, 4).map((image, index) => (
+							<ElementBox
+								key={index}
+								image={image}
+								description={descriptions[index]}
+								linkedin={linkedins[index]}
+								github={githubs[index]}
+								index={index}
+							/>
+						))}
+					</div>
+					<div className="elements-container second-row">
+						{images.slice(4, 7).map((image, index) => (
+							<ElementBox
+								key={index + 4}
+								image={image}
+								description={descriptions[index + 4]}
+								linkedin={linkedins[index + 4]}
+								github={githubs[index + 4]}
+								index={index + 4}
+							/>
 						))}
 					</div>
 				</div>
