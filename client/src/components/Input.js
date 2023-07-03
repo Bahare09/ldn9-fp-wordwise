636f101fb0892be6ecc0bee8e0531fbcf6b46e21
Bahare <108987748+Bahare09@users.noreply.github.com>
2023-07-03 23:58:04 +0100

    some updates for deploying (#61)

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -16,7 +16,7 @@ const Input = ({ onSubmit }) => {
 			return;
 		}
 		try {
-			const response = await fetch("http://localhost:3000/api", {
+			const response = await fetch("/api/correction", {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
