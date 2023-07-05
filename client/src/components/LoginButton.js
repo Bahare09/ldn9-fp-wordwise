8121f74e1037cce1564f4fb6b618228c0400630b
Bahare <itxbb09@gmail.com>
2023-07-05 12:08:54 +0100

    correction

--- client/src/components/LoginButton.js
+++ client/src/components/LoginButton.js
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
-import "./loginButton.css";
+//import "./LoginButton.css";
 
 const LoginButton = () => {
 	const { loginWithRedirect, isAuthenticated } = useAuth0();
