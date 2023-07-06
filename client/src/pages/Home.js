f0fbce0db88aa84507916e6c8c81c90669dfe6f7
Ying X <105919943+LexiisYing@users.noreply.github.com>
2023-07-06 17:03:13 +0100

    merge from main (#76)
    * fixed input/output and buttons inside them (#69)
    
    * deleted footer from mobile design (#71)
    
    * deleted footer from mobile design
    
    * Update App.js
    
    * added copyButton inside the alternative (#75)
    
    Co-authored-by: Bahare <108987748+Bahare09@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Zahraa <113244988+ZahraaTayyar@users.noreply.github.com>
    Co-authored-by: Hadi Allahverdi Pournigjeh <108987953+hadipournigjeh@users.noreply.github.com>
    Co-authored-by: Bahare <108987748+Bahare09@users.noreply.github.com>

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,6 +5,9 @@ import "./Home.css";
 import "../components/InputOutput.css";
 import Header from "../components/Header";
 import AlternativeButton from "../components/AlternativeButton";
+import CopyButton from "../components/CopyButton";
+import Footer from "../components/Footer";
+
 
 const Home = () => {
 	const [inputValue, setInputValue] = useState("");
@@ -47,11 +50,18 @@ const Home = () => {
 								Reset
 							</button>
 						</div>
-						<textarea
-							className="alternative-box"
-							value={alternativeValue}
-							readOnly
-						/>
+						<div className="alternative-wrap">
+							<div className="alternative-div">
+								<textarea
+									className="alternative-box"
+									value={alternativeValue}
+									readOnly
+								/>
+							</div>
+							<div className="CopyButton-div">
+								<CopyButton text={outputValue} />
+							</div>
+						</div>
 					</div>
 				</div>
 			);
@@ -67,19 +77,24 @@ const Home = () => {
 			<div className={isMobile ? "app" : "input-output-container"}>
 				{!isMobile && (
 					<div>
-						<div className="input-output-wrapper">
-							<div className="input-container">
-								<Input
-									inputValue={inputValue}
-									setInputValue={setInputValue}
-									onSubmit={handleSubmit}
-								/>
+						<div>
+							<div className="input-output-wrapper">
+								<div className="input-container">
+									<Input
+										inputValue={inputValue}
+										setInputValue={setInputValue}
+										onSubmit={handleSubmit}
+									/>
+								</div>
+								<div className="output-container">
+									<Output outputValue={outputValue} onReset={handleReset} />
+								</div>
 							</div>
-							<div className="output-container">
-								<Output outputValue={outputValue} onReset={handleReset} />
-							</div>
+							<div>{renderAlternatives()}</div>
 						</div>
-						<div>{renderAlternatives()}</div>
+						<div>
+							<Footer />
+						</div>
 					</div>
 				)}
 				{isMobile && !showOutput && (
