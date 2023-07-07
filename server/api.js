d4cd2ad043ade8b315eef55a6caac5d150c15a53
Bahare <itxbb09@gmail.com>
2023-07-07 21:39:53 +0100

    apply prettier

--- server/api.js
+++ server/api.js
@@ -26,14 +26,17 @@ router.post("/correction", async (req, res) => {
 				{
 					role: "user",
 					content: `
-You are a copywriter. Your job is to take some user text and make it better.
-- Improve the grammar
-- Make it more engaging 
-- Make it as concise as possible without sacrificing clarity
-The user typed the following:
-"${text}"
-Please re-write it, and make it better
-`,
+					You are a copywriter. Your job is to take some user text and make it better.
+					- Improve the grammar
+					- Make it more engaging 
+					- Make it as concise as possible without sacrificing clarity
+					
+					The user typed the following:
+					
+					"${text}"
+					
+					Please re-write it, and make it better
+					`,
 				},
 			],
 		});
@@ -81,11 +84,14 @@ router.post("/alternatives", async (req, res) => {
 				{
 					role: "user",
 					content: `
-You are a copywriter. Your job is to take some user text and offer alternative ways of phrasing it.
-The user typed the following:
-"${text}"
-Please provide three alternative ways to re-write it, and make it better
-`,
+					You are a copywriter. Your job is to take some user text and offer alternative ways of phrasing it.
+					
+					The user typed the following:
+					
+					"${text}"
+					
+					Please provide three alternative ways to re-write it, and make it better
+					`,
 				},
 			],
 		});
@@ -101,22 +107,6 @@ Please provide three alternative ways to re-write it, and make it better
 			.json({ error: "An error occurred while processing the request." });
 	}
 });
-// router.get("/test-db", async (req, res) => {
-// try {
-// // Hardcoded query to test the database connection
-// const query = "SELECT NOW() AS current_time";
-// const result = await db.query(query);
-
-// // Extract the current time from the result
-// const currentTime = result.rows[0].current_time;
-
-// res.json({ currentTime });
-// } catch (error) {
-// console.error("Error connecting to the database:", error);
-// res.status(500).json({ error: "Failed to connect to the database" });
-// }
-//});
-
 router.post("/saveUserData", async (req, res) => {
 	try {
 		const { name, email, sub } = req.body;
@@ -133,5 +123,19 @@ router.post("/saveUserData", async (req, res) => {
 		res.status(500).json({ error: "Failed to save user data" });
 	}
 });
+// router.get("/test-db", async (req, res) => {
+// try {
+// // Hardcoded query to test the database connection
+//    const query = "SELECT NOW() AS current_time";
+//    const result = await db.query(query);
 
+// // Extract the current time from the result
+//    const currentTime = result.rows[0].current_time;
+
+//     res.json({ currentTime });
+//     } catch (error) {
+//          console.error("Error connecting to the database:", error);
+//          res.status(500).json({ error: "Failed to connect to the database" });
+//   }
+//});
 export default router;
