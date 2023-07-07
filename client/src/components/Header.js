17a20a2ec48741910ed61c2e935a36b3335a73b4
Bahare <itxbb09@gmail.com>
2023-07-07 21:10:18 +0100

    implementing  post request to save userdata in database

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "./LoginButton";
@@ -13,18 +13,53 @@ const Header = ({ currentPage }) => {
 		setIsOpen(!isOpen);
 	};
 
+	const saveUserData = () => {
+		// Extract the necessary user data
+		const userData = {
+			name: user.name,
+			email: user.email,
+			sub: user.sub,
+		};
+
+		// Send the user data to the backend
+		fetch("/api/saveUserData", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(userData),
+		})
+			.then((response) => {
+				if (response.ok) {
+					console.log("User data saved successfully");
+				} else {
+					throw new Error("Failed to save user data");
+				}
+			})
+			.catch((error) => {
+				console.error("Error saving user data:", error);
+			});
+	};
+
+	useEffect(() => {
+		// Check if the user is authenticated
+		if (isAuthenticated) {
+			saveUserData();
+		}
+	}, [isAuthenticated]); // Run the effect when the isAuthenticated value changes
+
 	let navLinks =
 		currentPage === "home"
 			? [
 					<Link key="1" className="header-nav-link" to="/about">
 						About
 					</Link>,
-			]
+			  ]
 			: [
 					<Link key="1" className="header-nav-link" to="/">
 						Home
 					</Link>,
-			];
+			  ];
 
 	if (isAuthenticated) {
 		navLinks.push(
--- server/api.js
+++ server/api.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import logger from "./utils/logger";
 import { Configuration, OpenAIApi } from "openai";
-import db from "./db";
+import db from "./db"; // Import the db module
+
 const router = Router();
 
 router.get("/", (_, res) => {
@@ -25,23 +26,35 @@ router.post("/correction", async (req, res) => {
 				{
 					role: "user",
 					content: `
-					You are a copywriter. Your job is to take some user text and make it better.
-					- Improve the grammar
-					- Make it more engaging 
-					- Make it as concise as possible without sacrificing clarity
-					
-					The user typed the following:
-					
-					"${text}"
-					
-					Please re-write it, and make it better
-					`,
+You are a copywriter. Your job is to take some user text and make it better.
+- Improve the grammar
+- Make it more engaging 
+- Make it as concise as possible without sacrificing clarity
+The user typed the following:
+"${text}"
+Please re-write it, and make it better
+`,
 				},
 			],
 		});
 
 		if (completion.data.choices && completion.data.choices.length > 0) {
-			res.json(completion.data.choices[0].message.content);
+			const output = completion.data.choices[0].message.content;
+
+			// Store the output in the database
+			//await db.query("INSERT INTO history (output) VALUES ($1)", [output]);
+			// Store the output in the database
+			// Store the output in the database
+			try {
+				await db.query("INSERT INTO history (output) VALUES ($1)", [output]);
+			} catch (error) {
+				// Handle database query error
+				logger.error("Error inserting into the database:", error);
+				// You can choose to send an error response to the client here
+				// res.status(500).json({ error: "Failed to insert into the database" });
+			}
+			// Send the response
+			res.json(output);
 		} else {
 			res.status(404).json({ error: "No completion response received." });
 		}
@@ -68,14 +81,11 @@ router.post("/alternatives", async (req, res) => {
 				{
 					role: "user",
 					content: `
-					You are a copywriter. Your job is to take some user text and offer alternative ways of phrasing it.
-					
-					The user typed the following:
-					
-					"${text}"
-					
-					Please provide three alternative ways to re-write it, and make it better
-					`,
+You are a copywriter. Your job is to take some user text and offer alternative ways of phrasing it.
+The user typed the following:
+"${text}"
+Please provide three alternative ways to re-write it, and make it better
+`,
 				},
 			],
 		});
@@ -91,19 +101,36 @@ router.post("/alternatives", async (req, res) => {
 			.json({ error: "An error occurred while processing the request." });
 	}
 });
-router.get("/test-db", async (req, res) => {
+// router.get("/test-db", async (req, res) => {
+// try {
+// // Hardcoded query to test the database connection
+// const query = "SELECT NOW() AS current_time";
+// const result = await db.query(query);
+
+// // Extract the current time from the result
+// const currentTime = result.rows[0].current_time;
+
+// res.json({ currentTime });
+// } catch (error) {
+// console.error("Error connecting to the database:", error);
+// res.status(500).json({ error: "Failed to connect to the database" });
+// }
+//});
+
+router.post("/saveUserData", async (req, res) => {
 	try {
-		// Hardcoded query to test the database connection
-		const query = "SELECT NOW() AS current_time";
-		const result = await db.query(query);
+		const { name, email, sub } = req.body;
 
-		// Extract the current time from the result
-		const currentTime = result.rows[0].current_time;
+		// Store the user data in the database
+		await db.query(
+			"INSERT INTO users (name, email, google_id) VALUES ($1, $2, $3)",
+			[name, email, sub]
+		);
 
-		res.json({ currentTime });
+		res.status(200).json({ message: "User data saved successfully" });
 	} catch (error) {
-		console.error("Error connecting to the database:", error);
-		res.status(500).json({ error: "Failed to connect to the database" });
+		logger.error("Error saving user data:", error);
+		res.status(500).json({ error: "Failed to save user data" });
 	}
 });
 
