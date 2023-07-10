e70a10f1ed7bb0eaa859f611e42d7462af85bbe9
ZahraaTayyar <zahraatayyar@gmail.com>
2023-07-11 00:53:35 +0100

    alternatives styling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -44,7 +44,6 @@ const Home = () => {
 			});
 	};
 
-	
 	useEffect(() => {
 		const handleResize = () => {
 			setIsMobile(window.innerWidth < 768);
@@ -56,13 +55,11 @@ const Home = () => {
 		};
 	}, []);
 
-
 	const handleChange = (setter) => (event) => {
 		setter(event.target.value);
 	};
 	const onOutputValueChange = handleChange(setOutputValue);
 	const onAlternativeValueChange = handleChange(setAlternativeValue);
-
 
 	const handleSubmit = (data) => {
 		setOutputValue(data);
@@ -86,7 +83,7 @@ const Home = () => {
 			output: outputValue,
 			alternative: alternativeValue,
 		};
-		
+
 		saveUserData(userData);
 	};
 
@@ -94,36 +91,34 @@ const Home = () => {
 		if (outputValue) {
 			return (
 				<div className="alternative-container">
-					<div className="alternative-output">
-						<div>
-							<AlternativeButton
-								outputValue={outputValue}
-								setAlternativeValue={setAlternativeValue}
-								setShowAlternatives={setShowAlternatives}
-							/>
-							<button onClick={handleReset} className="reset-button">
-								Reset
-							</button>
-							<button onClick={handleSave} className="save-button">
-								Save
-							</button>
-						</div>
-						{showAlternatives && (
-							<div className="alternative-wrap">
-								<div className="alternative-div">
-									<textarea
-										className="alternative-box"
-										value={alternativeValue}
-										onChange={onAlternativeValueChange}
-									/>
-								</div>
-								<div className="CopyButton-div">
-									<CopyButton text={alternativeValue} />
-									<TextToSpeech outputValue={alternativeValue} />
-								</div>
-							</div>
-						)}
+					<div className="alternative-buttons-container">
+						<AlternativeButton
+							outputValue={outputValue}
+							setAlternativeValue={setAlternativeValue}
+							setShowAlternatives={setShowAlternatives}
+						/>
+						<button onClick={handleReset} className="reset-button">
+							Reset
+						</button>
+						<button onClick={handleSave} className="save-button">
+							Save
+						</button>
 					</div>
+					{showAlternatives && (
+						<div className="alternative-wrap">
+							<div className="alternative-div">
+								<textarea
+									className="alternative-box"
+									value={alternativeValue}
+									onChange={onAlternativeValueChange}
+								/>
+							</div>
+							<div className="CopyButton-div">
+								<CopyButton text={alternativeValue} />
+								<TextToSpeech outputValue={alternativeValue} />
+							</div>
+						</div>
+					)}
 				</div>
 			);
 		}
@@ -137,25 +132,25 @@ const Home = () => {
 			</div>
 			<div className={isMobile ? "app" : "input-output-container"}>
 				{!isMobile && (
-						<div className="input-output-alternatives-content">
-							<div className="input-output-wrapper">
-								<div className="input-container">
-									<Input
-										inputValue={inputValue}
-										setInputValue={setInputValue}
-										onSubmit={handleSubmit}
-									/>
-								</div>
-								<div className="output-container">
-									<Output
-										outputValue={outputValue}
-										onReset={handleReset}
-										onOutputValueChange={onOutputValueChange}
-									/>
-								</div>
+					<div className="input-output-alternatives-content">
+						<div className="input-output-wrapper">
+							<div className="input-container">
+								<Input
+									inputValue={inputValue}
+									setInputValue={setInputValue}
+									onSubmit={handleSubmit}
+								/>
 							</div>
-							<div>{renderAlternatives()}</div>
+							<div className="output-container">
+								<Output
+									outputValue={outputValue}
+									onReset={handleReset}
+									onOutputValueChange={onOutputValueChange}
+								/>
+							</div>
 						</div>
+						{renderAlternatives()}
+					</div>
 				)}
 				{isMobile && !showOutput && (
 					<div className="input-container">
