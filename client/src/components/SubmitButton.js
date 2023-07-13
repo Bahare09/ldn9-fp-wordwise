8081f3cb182eaac0fc9f39cd5fa4578168966d02
ZahraaTayyar <zahraatayyar@gmail.com>
2023-07-14 00:34:43 +0100

    Submit button position

--- client/src/components/SubmitButton.js
+++ client/src/components/SubmitButton.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./InputOutput.css";
 
-
 const SubmitButton = ({ onClick }) => {
 	return (
 		<div className="submit-btn-container">
 			<button type="submit" className="submit-button" onClick={onClick}>
-				&#10148;
+				Enter
 			</button>
 		</div>
 	);
