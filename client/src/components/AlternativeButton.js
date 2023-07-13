a03a54eb05bcec12de774a3adf0a60f168027da7
lexiisying <nyxxing0824@gmail.com>
2023-07-13 01:37:11 +0100

    extract api responses

--- client/src/components/AlternativeButton.js
+++ client/src/components/AlternativeButton.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./AlternativeButton.css";
 
-const AlternativeButton = ({ outputValue, setAlternativeValue, setShowAlternatives }) => {
+const AlternativeButton = ({
+	outputValue,
+	setAlternativeValues,
+	setShowAlternatives,
+}) => {
 	const getAlternatives = async () => {
 		try {
 			const response = await fetch("/api/alternatives", {
@@ -10,8 +14,12 @@ const AlternativeButton = ({ outputValue, setAlternativeValue, setShowAlternativ
 				body: JSON.stringify({ input: outputValue }),
 			});
 			if (response.ok) {
-				const alternative = await response.json();
-				setAlternativeValue(alternative);
+				const data = await response.json();
+				const alternatives = data
+					.split("\n")
+					.map((string) => string.replace(/^\d+\.\s/, "").replace(/[",]/g, ""))
+					.filter(Boolean);
+				setAlternativeValues(alternatives);
 				setShowAlternatives(true);
 			} else {
 				console.log("Error: " + response.status);
